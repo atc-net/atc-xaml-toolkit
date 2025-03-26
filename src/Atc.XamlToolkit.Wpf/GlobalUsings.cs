9c18911680d8b7d0f3df4a3ccf6b9edbb51b2f53
David Kallesen <david@lmdk.dk>
2025-03-26 12:34:26 +0100

    feat(xamltoolkit-wpf): add BindingErrorTraceListener and DesignModeHelper

--- src/Atc.XamlToolkit.Wpf/GlobalUsings.cs
+++ src/Atc.XamlToolkit.Wpf/GlobalUsings.cs
@@ -1,7 +1,10 @@
 // Global using directives
 
 global using System.ComponentModel;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Text;
 global using System.Windows;
 global using System.Windows.Input;
+global using System.Windows.Threading;
 global using Atc.XamlToolkit.Command;
