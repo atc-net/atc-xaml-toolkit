3ed6ccc6af5ca31252efdb915a6fe3154a1bc3e1
David Kallesen <david@lmdk.dk>
2025-04-22 23:59:37 +0200

    refac: move some logic to RelayCommandToGenerateExtensions

---
+++ src/Atc.XamlToolkit.SourceGenerators/Extensions/RelayCommandToGenerateExtensions.cs
@@ -0,0 +1,74 @@
+namespace Atc.XamlToolkit.SourceGenerators.Extensions;
+
+[SuppressMessage("Design", "MA0076:Do not use implicit culture-sensitive ToString in interpolated strings", Justification = "OK.")]
+internal static class RelayCommandToGenerateExtensions
+{
+    public static bool HasParameterTypesOfCancellationToken(
+        this RelayCommandToGenerate relayCommandToGenerate)
+    {
+        var parameterTypes = relayCommandToGenerate.ParameterTypes ?? [];
+        return parameterTypes.Any(t => t.EndsWith(NameConstants.CancellationToken, StringComparison.Ordinal));
+    }
+
+    public static string GetInterfaceType(
+        this RelayCommandToGenerate relayCommandToGenerate)
+        => relayCommandToGenerate.UseTask
+            ? NameConstants.IRelayCommandAsync
+            : NameConstants.IRelayCommand;
+
+    public static string GetImplementationType(
+        this RelayCommandToGenerate relayCommandToGenerate)
+        => relayCommandToGenerate.UseTask
+            ? NameConstants.RelayCommandAsync
+            : NameConstants.RelayCommand;
+
+    public static string GetCommandAsPropertyName(
+        this RelayCommandToGenerate relayCommandToGenerate)
+        => char.ToUpperInvariant(relayCommandToGenerate.CommandName[0]) + relayCommandToGenerate.CommandName.Substring(1);
+
+    public static string GetCommandAsFieldName(
+        this RelayCommandToGenerate relayCommandToGenerate)
+        => char.ToLowerInvariant(relayCommandToGenerate.CommandName[0]) + relayCommandToGenerate.CommandName.Substring(1);
+
+    public static string GetGenericArgAsString(
+        this RelayCommandToGenerate relayCommandToGenerate)
+    {
+        if (relayCommandToGenerate.ParameterValues?.Length > 0)
+        {
+            return string.Empty;
+        }
+
+        var filteredParameterTypes = relayCommandToGenerate.GetParameterTypesWithoutCancellationToken();
+
+        return filteredParameterTypes.Length switch
+        {
+            0 => string.Empty,
+            1 => $"<{filteredParameterTypes[0]}>",
+            _ => $"<({string.Join(", ", filteredParameterTypes)})>",
+        };
+    }
+
+    public static string[] GetParameterTypesWithoutCancellationToken(
+        this RelayCommandToGenerate relayCommandToGenerate)
+    {
+        var parameterTypes = relayCommandToGenerate.ParameterTypes ?? [];
+        return parameterTypes
+            .Where(t => !t.EndsWith(NameConstants.CancellationToken, StringComparison.Ordinal))
+            .ToArray();
+    }
+
+    public static string? GetParameterValuesAsCommaSeparated(
+        this RelayCommandToGenerate relayCommandToGenerate)
+        => relayCommandToGenerate.ParameterValues?.Length > 0
+            ? string.Join(", ", relayCommandToGenerate.ParameterValues)
+            : null;
+
+    public static string? GetParameterTypesWithoutCancellationTokenAsItemNumberArgsAsCommaSeparated(
+        this RelayCommandToGenerate relayCommandToGenerate)
+    {
+        var filteredParameterTypes = relayCommandToGenerate.GetParameterTypesWithoutCancellationToken();
+        return filteredParameterTypes.Length > 0
+            ? string.Join(", ", filteredParameterTypes.Select((_, i) => $"x.Item{i + 1}"))
+            : null;
+    }
+}
